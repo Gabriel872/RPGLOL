8261bcbd026603bbe73e08dde2e99f883f365d37
Gabriel872 <54552055+Gabriel872@users.noreply.github.com>
2021-04-07 00:09:57 -0300

    Version 0.27
    - New UI add in test scene;
    - More organized folders;

---
+++ RPG LOL/Assets/Scripts/TestScene/windowControl.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class windowControl : MonoBehaviour
+{
+    [SerializeField] private GameObject window;
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = window.GetComponent<Animator>();
+    }
+
+    public void WidowOperation()
+    {
+        if (window.activeSelf)
+        {
+            Close();
+        }
+        else
+        {
+            Open();
+        }
+    }
+
+    private void Open()
+    {
+        if (anim == null)
+        {
+            return;
+        }
+        else
+        {
+            window.SetActive(true);
+            anim.Play("Window");
+        }
+    }
+
+    private void Close()
+    {
+        if (anim == null)
+        {
+            return;
+        }
+        else
+        {
+            anim.Play("windowClose");
+            Invoke("FinishOperations", 1f);
+        }
+    }
+
+    private void FinishOperations()
+    {
+        window.SetActive(false);
+    }
+}
