53c2bf6223d6e38d812b45956c32c6e3cd9dead2
Gabriel872 <54552055+Gabriel872@users.noreply.github.com>
2021-04-07 15:24:26 -0300

    0.27.1
    -Small Changes in input on players UI;

--- RPG LOL/Assets/Scripts/StatusManager.cs
+++ RPG LOL/Assets/Scripts/StatusManager.cs
@@ -6,8 +6,16 @@ using TMPro;
 public class StatusManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _life;
+    [SerializeField] private TMP_InputField _textInput;
     [SerializeField] private SpriteRenderer _sprPlayer;
     [SerializeField] private Sprite _spr;
+
+    public void SetNull()
+    {
+        _life.text = "";
+        _textInput.text = "";
+        _textInput.Select();
+    }
 
     public void ChangeLife(TMP_InputField _input)
     {
