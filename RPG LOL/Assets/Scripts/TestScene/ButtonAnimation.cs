9491318af4da4366098f5e96d8518e06c561e6bd
Gabriel872 <54552055+Gabriel872@users.noreply.github.com>
2021-04-08 00:03:58 -0300

    0.27.2
    - Little changes in UI;

---
+++ RPG LOL/Assets/Scripts/TestScene/ButtonAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAnimation : MonoBehaviour
+{
+    public Animator animButton;
+    public bool huge;
+
+    public void GrowUp()
+    {
+        if (!huge)
+        {
+            animButton.Play("animButton");
+            huge = true;
+        }
+        else
+        {
+            animButton.Play("animButton2");
+            huge = false;
+        }
+    }
+
+    public void Idle()
+    {
+        animButton.Play("idleButton");
+    }
+}
